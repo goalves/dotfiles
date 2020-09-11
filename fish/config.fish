9c845f9342457bf8592a901a81f9433a5602a3c2
Gabriel Alves <g04lves@gmail.com>
2020-09-10 22:07:34 -0300

    Add cargo bin folder to fish user path

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 set -e fish_user_paths
 set -U fish_user_paths "/usr/local/sbin" $fish_user_paths
 set -U fish_user_paths "/usr/local/bin" $fish_user_paths
-
+set -U fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
 set -U EDITOR vim
 
 eval (starship init fish)
