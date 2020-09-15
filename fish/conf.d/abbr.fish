8074fa1a4f61d7d34c003c307d75d851bf5bd6ca
Gabriel Alves <g04lves@gmail.com>
2020-09-15 14:46:17 -0300

    Add abbreviations

---
+++ fish/conf.d/abbr.fish
@@ -0,0 +1,9 @@
+abbr -a -U -- dev 'cd ~/Projects'
+abbr -a -U -- grep 'grep --color=auto'
+abbr -a -U -- iex 'iex --erl "-kernel shell_history enabled"'
+abbr -a -U -- kube-unset 'kubectl config unset current-context'
+abbr -a -U -- mf 'mix format'
+abbr -a -U -- mt 'mix test'
+abbr -a -U -- mtf 'mix test --failed'
+abbr -a -U -- mtw 'mix test --only wip'
+abbr -a -U -- weather curl\ -4\ \'http://wttr.in/Porto+Alegre\?m
