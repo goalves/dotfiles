aa8ea93e13f4f4b20443a43ff04d163db5f7e68b
Gabriel Alves <g04lves@gmail.com>
2020-09-16 15:00:54 -0300

    Add local brew install of mysql path to fish config

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,7 @@ set -U EDITOR vim
 
 eval (starship init fish)
 
-# The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/goalves/Downloads/google-cloud-sdk 2/path.fish.inc' ]; . '/Users/goalves/Downloads/google-cloud-sdk 2/path.fish.inc'; end
 
 source /usr/local/opt/asdf/asdf.fish
+set -g fish_user_paths "/usr/local/opt/mysql@5.6/bin" $fish_user_paths
