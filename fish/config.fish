6522591e29cb11e3003e9ee71f90fe7bc4f127f0
Gabriel Alves <g04lves@gmail.com>
2020-10-26 12:10:34 -0300

    Add poetry to manage python dependencies

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,7 @@ set -e fish_user_paths
 set -U fish_user_paths "/usr/local/sbin" $fish_user_paths
 set -U fish_user_paths "/usr/local/bin" $fish_user_paths
 set -U fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
+set -U fish_user_paths "$HOME/.poetry/bin" $fish_user_paths
 set -U EDITOR vim
 
 eval (starship init fish)
