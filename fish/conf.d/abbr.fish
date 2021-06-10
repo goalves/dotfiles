10796a9abd23af8c9823bde7608482069e3ff2bd
Gabriel Alves <g04lves@gmail.com>
2021-06-10 14:17:28 -0300

    Update configs to work with xclip and xsel on linux

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -7,3 +7,5 @@ abbr -a -U -- mt 'mix test'
 abbr -a -U -- mtf 'mix test --failed'
 abbr -a -U -- mtw 'mix test --only wip'
 abbr -a -U -- weather curl\ -4\ \'http://wttr.in/Porto+Alegre\?m
+abbr -a -U -- pbcopy 'xclip -selection clipboard'
+abbr -a -U -- pbpaste 'xclip -selection clipboard -o'
