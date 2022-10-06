4d1fd3225dc65279162ca07d351682eaafd04a5b
Gabriel Alves <g04lves@gmail.com>
2022-10-06 11:46:58 -0700

    update fish and alacritty configs to work with linux and remove mac crap

--- fish/config.fish
+++ fish/config.fish
@@ -4,13 +4,8 @@ set -U fish_user_paths "/usr/local/bin" $fish_user_paths
 set -U fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
 set -U fish_user_paths "$HOME/.poetry/bin" $fish_user_paths
 set -U EDITOR vim
+set fish_greeting
+
+source /opt/asdf-vm/asdf.fish
 
 eval (starship init fish)
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/goalves/Downloads/google-cloud-sdk 2/path.fish.inc' ]; . '/Users/goalves/Downloads/google-cloud-sdk 2/path.fish.inc'; end
-
-source /usr/local/opt/asdf/asdf.fish
-set -g fish_user_paths "/usr/local/opt/mysql@5.6/bin" $fish_user_paths
-
-source /usr/local/opt/asdf/asdf.fish
