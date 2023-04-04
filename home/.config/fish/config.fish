1d8ca8f3c2298229004d262dee0891349a8f72f0
Gabriel Alves <g04lves@gmail.com>
2023-04-04 13:10:32 -0300

    add missing libs installed from homebrew

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -3,6 +3,9 @@ fish_add_paths "/usr/local/sbin"
 fish_add_paths "/usr/local/bin"
 fish_add_paths "/opt/homebrew/bin"
 fish_add_paths "$HOME/.cargo/bin"
+fish_add_path "/opt/homebrew/opt/postgresql@15/bin"
+fish_add_path "/opt/homebrew/opt/libpq/bin"
+
 set EDITOR vim
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
