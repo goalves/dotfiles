b9675b91a83e7ff5614991dd5a7f6392012fb4de
Gabriel Alves <g04lves@gmail.com>
2023-05-03 15:00:48 -0300

    feat: update fish paths and git default branch

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,8 +1,7 @@
-
-fish_add_paths "/usr/local/sbin"
-fish_add_paths "/usr/local/bin"
-fish_add_paths "/opt/homebrew/bin"
-fish_add_paths "$HOME/.cargo/bin"
+fish_add_path "/usr/local/sbin"
+fish_add_path "/usr/local/bin"
+fish_add_path "/opt/homebrew/bin"
+fish_add_path "$HOME/.cargo/bin"
 fish_add_path "/opt/homebrew/opt/postgresql@15/bin"
 fish_add_path "/opt/homebrew/opt/libpq/bin"
 
