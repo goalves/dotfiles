f6f54d9d8f6deb82be6920a4cbaa4e023ea98494
Gabriel Alves <g04lves@gmail.com>
2023-10-18 17:02:36 -0300

    update documentation, setup and move back to alacritty since warp is slow

--- README.md
+++ README.md
@@ -5,6 +5,14 @@ I am currently using MacOS.
 
 ## MacOS setup
 
-- Install Warp
+- Install Alacritty (other option would be to use Warp but its slow)
 - Install GPGSuite
 - Follow script on setup.sh
+
+## Tmux
+
+If tmux plugins are not being installed for some reason, you should run:
+
+```sh
+~/.tmux/plugins/tpm/bin/install_plugins
+```
--- setup.sh
+++ setup.sh
@@ -11,19 +11,32 @@ brew install --cask font-fira-code
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 brew install fish helix tmux btop tig neovim coreutils curl git bat alacritty postgresql@15 libpq fnm
 
+# Install oh my fish
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+
+# Install Rust
 curl -sS https://starship.rs/install.sh | sh
 
+# Install ASDF
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.11.3
-mkdir -p ~/.config/fish/completions
-ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
 
 # Setup Projects folder and clone dotfiles
 mkdir ~/Projects && cd ~/Projects
 git clone https://github.com/goalves/dotfiles
+
+# Install TPM
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Install nerdfonts
+brew tap homebrew/cask-fonts
+brew install font-hack-nerd-font
 
 # Setup ASDF elixir and erlang
 asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
 asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
 
 # Now manually install files to their locations
+
+# Configure Fish
+mkdir -p ~/.config/fish/completions
+ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
