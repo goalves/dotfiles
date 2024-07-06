f20e696c7361684297ccf017591fd6d4cf8a4613
Gabriel Alves <g04lves@gmail.com>
2024-07-06 12:12:25 -0300

    feat: add extra leaves programs and update asdf on setup script

--- setup.sh
+++ setup.sh
@@ -10,11 +10,11 @@ xargs brew install < leaves.txt
 # Install oh my fish
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 
-# Install Rust
+# Install Starship
 curl -sS https://starship.rs/install.sh | sh
 
 # Install ASDF
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.11.3
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
 
 # Setup Projects folder and clone dotfiles
 mkdir ~/Projects && cd ~/Projects
@@ -26,6 +26,9 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # Setup ASDF elixir and erlang
 asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
 asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
+
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # Configure Fish
 mkdir -p ~/.config/fish/completions
