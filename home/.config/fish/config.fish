21963623321f2067313f1ded05a7b4853916a486
Gabriel Alves <g04lves@gmail.com>
2025-05-08 10:15:54 -0300

    fix: add go install path to use it without homebrew

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,6 +1,7 @@
 fish_add_path "/usr/local/sbin"
 fish_add_path "/usr/local/bin"
 fish_add_path "/opt/homebrew/bin"
+fish_add_path "/usr/local/go/bin"
 fish_add_path "$HOME/.cargo/bin"
 fish_add_path "/opt/homebrew/opt/postgresql@15/bin"
 fish_add_path "/opt/homebrew/opt/libpq/bin"
